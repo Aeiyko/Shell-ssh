18612171495ef77761c8576d165ad2e1d365c8b7
Alexis SALVETTI <alexis_salvetti@ens.univ-artois.fr>
2020-11-07 14:25:15 +0100

    :sparkles: - Ajout de la fonctionnalité && et || dans le shell

--- src/commands.c
+++ src/commands.c
@@ -11,8 +11,10 @@
 #define CYANF "\033[0m"
 #define CH_LENGTH 1024
 #define OPT_LENGTH 42
-#define SEP ";"
+#define PTV ";"
 #define SPACE ' '
+#define ET "&&"
+#define OU "||"
 
 
 char * readCmd(){ // Lis une ligne entière
@@ -67,14 +69,43 @@ char **recupCmd(char *command){
     return cmd;
 }
 
+int boucleCmd(char **cmd,int *sortie){
+    int status,finish=1;
+
+    pid_t pid;
+
+
+    if(!strcmp(cmd[0], "exit")){ // Si exit
+        *sortie = 0;
+        exit(NO_ERR);
+    }
+    if ( (pid = fork()) == ERR ) syserror(FORK_ERR);
+    else if (!pid) {
+        execCmd(cmd); // Execute la commande
+    }
+    wait(&status);
+    if (WIFEXITED(status) && WEXITSTATUS(status)!=0){
+        //Changer la couleur du prompt
+        finish = 0;
+    }
+    write(STDOUT_FILENO , "\n", sizeof(char));
+    return finish;
+}
+
+int find(char **tab, char *ch){
+    for (int i=0; tab[i]; i++) {
+        if(!strcmp(tab[i], ch))return i;
+    }
+    return -1;
+}
 
 int main(void){
     
-    pid_t pid;
-    int status;
     char *ligne_cmd;
     char *tmp_cmd;
     char **cmd;
+    char *separateur = PTV;
+    int test_cmd,test_et = 0,test_ou = 0,test_find_et,test_find_ou;
 
     int sortie=1;
     while (sortie) {
@@ -85,28 +116,57 @@ int main(void){
         write(STDOUT_FILENO, CYANF, sizeof(char)*4);
         free(path);
 
+        tmp_cmd = NULL;
+        ligne_cmd = NULL;
         ligne_cmd = readCmd(); // Recup la ligne
-        tmp_cmd = strtok(ligne_cmd,SEP);
+        tmp_cmd = strtok(ligne_cmd,separateur);
+
+        cmd = NULL;
+        test_et = 0;
+        test_ou = 0;
+        
+
 
         while (tmp_cmd) { // Tant qu'il y a des commandes dans la ligne
-            cmd = recupCmd(tmp_cmd); // Transforme la ligne en tableau
-            if(!strcmp(cmd[0], "exit")){ // Si exit
-                sortie = 0;
-                exit(NO_ERR);
+            if (!test_et && !test_ou) {            
+                cmd = recupCmd(tmp_cmd); // Transforme la ligne en tableau
             }
-            if ( (pid = fork()) == ERR ) syserror(FORK_ERR);
-            else if (!pid) {
-                execCmd(cmd); // Execute la commande
+            test_ou = 0;
+            test_et = 0;
+            if ( (test_find_et = find(cmd,ET)) >= 0) {
+                test_et = 1;
+                cmd[test_find_et] = NULL;
             }
-            wait(&status);
-            if (WIFEXITED(status) && WEXITSTATUS(status)!=0){
-                //Changer la couleur du prompt
+            if ( (test_find_ou = find(cmd,OU)) >= 0) {
+                test_ou = 1;
+                cmd[test_find_ou] = NULL;
+
             }
-            write(STDOUT_FILENO , "\n", sizeof(char));
-            tmp_cmd = strtok(NULL, SEP);    
+            test_cmd = boucleCmd(cmd, &sortie);
+            if(test_et){
+                if(test_cmd){
+                    cmd = &(cmd[test_find_et+1]);
+                }
+                else{
+                    tmp_cmd = strtok(NULL, separateur);
+                }
+            }
+            else if(test_ou){
+                if (!test_cmd) {
+                    cmd = &(cmd[test_find_ou+1]);     
+                }
+                else {
+                    tmp_cmd = strtok(NULL, separateur);
+                }
+            }
+            else {            
+                tmp_cmd = strtok(NULL, separateur);
+            }
         }
     }
 
 
     return 0;
+    // Note todo : au lieu de faire un find ... faire que si on tombe sur une chaine && ou || regarde l etat de la derniere commande
+    // et voit si on doit faire la prochaine
 }
