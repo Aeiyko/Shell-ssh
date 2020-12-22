623837df25169e5d70806985930676c8cf05e52a
Alexis SALVETTI <alexis_salvetti@ens.univ-artois.fr>
2020-12-22 16:17:27 +0100

    Changement dans le parser qui provoquait des warnnings

--- src/commande/parser.h
+++ src/commande/parser.h
@@ -7,34 +7,6 @@ enum sep{ET,OU,PTV,PIPE,BACK};
 enum red{STD,STDERR,BOTH,INJ};
 enum redtype{SIMPLE,ADD};
 
-static char *typestring[] = {
-    "CMD",
-    "SEP",
-    "RED",
-    "VAR"
-};
-
-static char *sepstring[] = {
-    "ET",
-    "OU",
-    "PTV",
-    "PIPE",
-    "BACK"
-};
-
-static char *redstring[] = {
-    "STD",
-    "STDERR",
-    "BOTH",
-    "INJ"
-};
-
-static char *redtypestring[] = {
-    "SIMPLE",
-    "ADD"
-};
-
-
 struct parser{
     enum type type;
     enum sep sep;
